a0a5949c53e7dbfb4ec38b1f20cf594fe320d0cb
mike rapp & ashlin aronin <student@epicodus.com>
2015-08-12 12:10:16 -0700

    Clean up variable names

--- app/app.php
+++ app/app.php
@@ -18,8 +18,12 @@
     $app->get("/shoot", function() use($app)
     {
         $my_RPSGame = new RPSGame;
-        $winner = $my_RPSGame->chooseWinner($_GET['Player1'], $_GET['Player2']);
-        return $app['twig']->render('shoot.html.twig', array('winner' => $winner));
+        $winner = $my_RPSGame->chooseWinner($_GET['player1'], $_GET['player2']);
+        return $app['twig']->render('shoot.html.twig', array(
+            'winner' => $winner,
+            'player1' => $_GET['player1'],
+            'player2' => $_GET['player2']
+        ));
     });
 
     // Play computer form
@@ -33,8 +37,12 @@
         $my_RPSGame = new RPSGame;
         $options = array("Rock", "Paper", "Scissors");
         $computer = $options[rand(0,2)];
-        $winner = $my_RPSGame->chooseWinner($_GET['Player1'], $computer);
-        return $app['twig']->render('shoot_computer.html.twig', array('winner' => $winner, 'player1' => $_GET['Player1'], 'computer' => $computer));
+        $winner = $my_RPSGame->chooseWinner($_GET['player1'], $computer);
+        return $app['twig']->render('shoot_computer.html.twig', array(
+            'winner' => $winner,
+            'player1' => $_GET['player1'],
+            'player2' => $computer
+        ));
     });
 
     return $app;
