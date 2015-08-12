57addd6c0ed9eab163ed1f62f6230cc4fd370ea1
mike rapp & ashlin aronin <student@epicodus.com>
2015-08-12 10:34:55 -0700

    Add draw spec and make it fail

--- tests/RPSGameTest.php
+++ tests/RPSGameTest.php
@@ -4,7 +4,16 @@
 
     class RPSGameTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_chooseWinner_sameInputs()
+        {
+            $test_RPSGame = new RPSGame;
+            $player1 = "Rock";
+            $player2 = "Rock";
+
+            $result = $test_RPSGame->chooseWinner($player1, $player2);
+
+            $this->assertEquals("Draw", $result);
+        }
     }
 
  ?>
