f4f268f97f12e41acf8bd8739ce4368d7cc0526b
mike rapp & ashlin aronin <student@epicodus.com>
2015-08-12 12:01:18 -0700

    add the resulting inputs

--- app/app.php
+++ app/app.php
@@ -34,7 +34,7 @@
         $options = array("Rock", "Paper", "Scissors");
         $computer = $options[rand(0,2)];
         $winner = $my_RPSGame->chooseWinner($_GET['Player1'], $computer);
-        return $app['twig']->render('shoot_computer.html.twig', array('winner' => $winner));
+        return $app['twig']->render('shoot_computer.html.twig', array('winner' => $winner, 'player1' => $_GET['Player1'], 'computer' => $computer));
     });
 
     return $app;
